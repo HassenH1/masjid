a3706b780c0d801115683e7d251870b29154a549
HassenH1 <hasansaid51@gmail.com>
2021-07-24 11:53:51 -0700

    ...

--- server.js
+++ server.js
@@ -19,15 +19,15 @@ app.use(express.json());
 // override with POST having ?_method=DELETE
 app.use(methodOverride("_method"));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "build")));
-  // app.get("*", (res,req) => {
-  //   req.sendFile(path.resolve(__dirname, "build", "index.html"))
-  // })
-  app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+app.use(express.static(path.join(__dirname, "build")));
+// app.get("*", (res,req) => {
+//   req.sendFile(path.resolve(__dirname, "build", "index.html"))
+// })
+// app.get("/*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "build", "index.html"));
+// });
+//}
 
 app.get("/", async (req, res) => {
   try {
