6b800862a29b50ef8e2a53085b4ffd29a7c35c5c
HassenH1 <hasansaid51@gmail.com>
2021-10-09 14:51:44 -0700

    testing for now

--- src/component/paypal/Paypal.js
+++ src/component/paypal/Paypal.js
@@ -1,54 +1,79 @@
 import React, { useState, useEffect, useRef } from "react";
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
+import { Container } from "react-bootstrap";
 
 function Paypal(props) {
-  const { amount, customAmount } = props;
-  const [paid, setPaid] = useState(false);
-  const [error, setError] = useState(null);
-  const paypalRef = useRef();
+  const initialOptions = {
+    "client-id": "test",
+    currency: "USD",
+    intent: "capture",
+    "data-client-token":
+      "AXMahTmR0JckXdm5Xu3wFfnQKy2fI-iobom9GM1M9zg2wBDNrpYqO31iuXuS6PDRsy_vAWaSzy7ZauYC",
+  };
 
-  useEffect(() => {
-    let myButton = window.paypal.Buttons({
-      createOrder: (data, actions) => {
-        return actions.order.create({
-          intent: "CAPTURE",
-          purchase_units: [
-            {
-              description: "",
-              amount: {
-                currency_code: "USD",
-                value: amount !== "other" ? amount : customAmount,
-              },
+  const [amount, setAmount] = useState("2.00");
+  const [orderID, setOrderID] = useState("");
+  const [onApproveMessage, setOnApproveMessage] = useState("");
+  const [onErrorMessage, setOnErrorMessage] = useState("");
+
+  function createOrder(data, actions) {
+    return actions.order
+      .create({
+        purchase_units: [
+          {
+            amount: {
+              value: amount,
             },
-          ],
-        });
-      },
-      onApprove: async (data, actions) => {
-        const order = await actions.order.capture();
-        setPaid(true);
-        console.log(order);
-      },
-      onError: (err) => {
-        setError(err);
-        return err;
-      },
-    });
+          },
+        ],
+      })
+      .then((orderID) => {
+        setOrderID(orderID);
+        return orderID;
+      });
+  }
 
-    myButton.render(paypalRef.current);
+  function onApprove(data, actions) {
+    console.log(data, "<-0-0-0-0-0what is data?");
+    console.log(actions, "<-0-0-0-0-0what is actions?");
+    // return actions.order.capture().then(function (details) {
+    //   setOnApproveMessage(
+    //     `Transaction completed by ${details.payer.name.given_name}!`
+    //   );
+    // });
+  }
 
-    return () => {
-      myButton.close();
-    };
-  }, []);
+  function onError(err) {
+    setOnErrorMessage(err.toString());
+  }
+
+  function onChange(event) {
+    setAmount(event.target.value);
+    setOrderID("");
+    setOnApproveMessage("");
+    setOnErrorMessage("");
+  }
 
   return (
-    <div>
-      {paid ? <div>Payment successful!</div> : null}
-      {error ? (
-        <div>Error Occurred in processing payment! Please try again later.</div>
-      ) : null}
-      <h4>Total Amount: ${amount !== "other" ? amount : customAmount}</h4>
-      <div ref={paypalRef} />
+    // <Container className="border">
+    <div className="container-xxl border">
+      <PayPalScriptProvider
+        options={{
+          "client-id": "test",
+          currency: "USD",
+          // "data-client-token":
+          //   "AXMahTmR0JckXdm5Xu3wFfnQKy2fI-iobom9GM1M9zg2wBDNrpYqO31iuXuS6PDRsy_vAWaSzy7ZauYC",
+        }}
+      >
+        <PayPalButtons
+          createOrder={createOrder}
+          onApprove={onApprove}
+          onError={onError}
+          forceReRender={[amount]}
+        />
+      </PayPalScriptProvider>
     </div>
+    // </Container>
   );
 }
 
