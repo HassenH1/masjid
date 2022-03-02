3d54504d96b2ccc719e5c0a63547d27bae7c1403
HassenH1 <hasansaid51@gmail.com>
2022-03-01 18:49:28 -0800

    added paypal button

--- src/component/navbar/Navbar.js
+++ src/component/navbar/Navbar.js
@@ -55,9 +55,9 @@ const NavbarComponent = () => {
                   GoFundMe
                 </a>
               </li>
-              {/* <li className="nav-item">
+              <li className="nav-item">
                 <span className="nav-link">
-                  <form
+                  {/* <form
                     action="https://www.paypal.com/donate"
                     method="post"
                     target="_top"
@@ -85,9 +85,42 @@ const NavbarComponent = () => {
                     >
                       Donate
                     </button>
+                  </form> */}
+                  <form
+                    action="https://www.paypal.com/donate"
+                    method="post"
+                    target="_top"
+                  >
+                    <input
+                      type="hidden"
+                      name="business"
+                      value="94QUZPMJYJCKL"
+                    />
+                    <input type="hidden" name="no_recurring" value="1" />
+                    <input
+                      type="hidden"
+                      name="item_name"
+                      value="Masjid Faatimah Donation "
+                    />
+                    <input type="hidden" name="currency_code" value="USD" />
+                    <input
+                      type="image"
+                      src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif"
+                      border="0"
+                      name="submit"
+                      title="PayPal - The safer, easier way to pay online!"
+                      alt="Donate with PayPal button"
+                    />
+                    <img
+                      alt=""
+                      border="0"
+                      src="https://www.paypal.com/en_US/i/scr/pixel.gif"
+                      width="1"
+                      height="1"
+                    />
                   </form>
                 </span>
-              </li> */}
+              </li>
             </ul>
           </div>
         </div>
@@ -97,3 +130,5 @@ const NavbarComponent = () => {
 };
 
 export default NavbarComponent;
+
+//generate QR code for masjid!!
